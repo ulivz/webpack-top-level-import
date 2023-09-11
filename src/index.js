070b5774eb1f1be26f8f8a5db1c4b580cb40e360
chenhaoli <chenhaoli@bytedance.com>
2023-09-11 14:17:16 +0800

    chore: tweaks

--- src/index.js
+++ src/index.js
@@ -1,14 +1,9 @@
-function sleep(t) {
-  return new Promise((resolve) => {
-    setTimeout(resolve, t);
-  });
-}
-
-function component() {
+export function component() {
   const element = document.createElement("div");
   element.innerHTML = "Hello, Webpack!";
   return element;
 }
 
-await sleep(1000);
+await 1000;
+
 document.body.appendChild(component());
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,20 @@
 const path = require("path");
 
+/**
+ * @type {import('webpack').Configuration}
+ */
 module.exports = {
   mode: "production",
   entry: "./src/index.js",
+  devtool: 'source-map',
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
   },
+  experiments: {
+    topLevelAwait: true,
+  },
+  optimization: {
+    minimize: false
+  }
 };
